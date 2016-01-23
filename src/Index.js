d9e46bf955d34b4da8b2b5957743274e3eb91cc5
Ali Waseem <waseema393@gmail.com>
2016-01-22 18:47:37 -0700

    added webpack config

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,13 @@
 import "whatwg-fetch";
-import $ from "jquery";
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-
-window.jQuery = $;
+// import all css files into entry point
+import "../public/normalize.css";
+import "../public/skeleton.css";
+import "../public/main.css";
+// Import React components
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
 
 window.React = React;
 
-render( <App/>, document.getElementById('content'));
+render( <App/>, document.getElementById("content"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     entry: [
+        "webpack-dev-server/client?http://localhost:8080",
+        "webpack/hot/only-dev-server",
         "./src/Index.js"
     ],
     module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: "babel"
+            loader: "react-hot!babel"
+        }, {
+            test: /\.css$/, // Only .css files
+            loader: "react-hot!style!css" // Run both loaders
         }]
     },
     resolve: {
@@ -18,6 +26,14 @@ module.exports = {
         filename: "bundle.js"
     },
     devServer: {
-        contentBase: "./dist"
-    }
+        contentBase: "./dist",
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html"
+        })
+    ]
 };
